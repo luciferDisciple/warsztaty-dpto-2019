1d84d5b3cd035cbbb9d3a5d9bc1eb5ad0131e5b4
luciferDisciple <waizer12@gmail.com>
2019-11-30 12:35:12 +0100

    There are multiple modules with "names that only differ in casing." fix

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from '../components/header';
-import { Contact } from '../components/contact';
+import { Header } from '../components/Header';
+import { Contact } from '../components/Contact';
 
 export const Layout = ({ children }) =>
     <>
