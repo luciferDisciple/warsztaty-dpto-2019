35bfdd37ef5a0ecd327eac770abedd6e4487720a
luciferDisciple <waizer12@gmail.com>
2019-11-30 13:26:26 +0100

    Created "/blog" page.

---
+++ src/pages/blog.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Layout from '../containers/Layout';
+import { graphql } from "gatsby"
+
+export default ({
+    data: {
+        allMarkdownRemark: { edges },
+    },
+}) => {
+    return (
+        <Layout>
+        </Layout>
+    );
+}
+
+// analogiczne do:
+// const {title, content} = props;
+//
+// ^ restrukturyzacja obiektu
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "YYYY-MM-DD")
+            path
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
