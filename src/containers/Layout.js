2f99a073ae68326076e75659c6a4a4217c89789b
luciferDisciple <waizer12@gmail.com>
2019-11-30 11:45:52 +0100

    Created and used Layout container.

---
+++ src/containers/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from '../components/header';
+import { Contact } from '../components/contact';
+
+export const Layout = ({ children }) =>
+    <>
+        <Header />
+        {children}
+        <Contact />
+    </>
